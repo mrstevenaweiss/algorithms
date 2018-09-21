7b0f094d7c506d2b2d8d9599041076275c749b27
mrstevenaweiss <weiss.steven@gmail.com>
2018-09-20 22:54:48 -0400

    added gauss-sum

---
+++ gauss-sum.py
@@ -0,0 +1,11 @@
+
+# Gauss Summation Trick
+
+while True:
+    print('Sum all the digits up to N: ')
+    print("Type 'exit()' to quit \n")
+
+    number = int(input())
+
+    total = (number/2)*(number+1)
+    print("Sum of all numbers up to " + str(number) + " = " + str(total) + "\n")
