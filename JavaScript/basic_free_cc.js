01f3f9c543e142809bfa098abc39a87edeed3f8f
mrstevenaweiss <weiss.steven@gmail.com>
2019-03-27 17:51:21 -0400

    added new js

--- JavaScript/basic_free_cc.js
+++ JavaScript/basic_free_cc.js
@@ -11,3 +11,52 @@ function repeatStringNumTimes(str, num) {
 
 console.log(repeatStringNumTimes("abc", -1))
 ;
+
+
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a// ... ending.
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (num < str.length) {
+    return str.slice(0, num)+'...';
+  } else {
+    return str;
+  }
+}
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+
+
+
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+
+function findElement(arr, func) {
+  let numbers = arr.filter(func);
+  return numbers[0];
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+
+//Check if a value is classified as a boolean primitive. Return true or false.
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return typeof bool == 'boolean';
+}
+
+booWho(false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
