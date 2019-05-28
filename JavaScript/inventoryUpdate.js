858856cad8893ff649084925fb1e34465c061884
mrstevenaweiss <weiss.steven@gmail.com>
2019-05-28 15:32:46 -0400

    yo

---
+++ JavaScript/inventoryUpdate.js
@@ -0,0 +1,30 @@
+
+
+for (let idx in newInv) {
+//   let newItem = newInv[idx][1]
+//   let currentInv = newInv[idx][0]
+
+//   let counter = 0
+//   for (let innerIdx in curInv) {
+//     let oldItem = curInv[innerIdx][1] 
+//     let oldInv = curInv[innerIdx][0]
+//     console.log(oldInv)
+
+//     console.log('new:  ', newItem, 'old:  ', oldItem)
+//     counter++
+//     // console.log(counter)
+
+//     if (newItem == oldItem) {
+//       let newQuant = currentInv + oldInv
+//       console.log(currentInv, oldInv, newQuant)
+//       console.log(curInv[idx][0])
+//       curInv[idx][0] = newQuant
+//       counter = 0
+//       break
+//     } 
+//   }
+//   if (counter > 0) {
+//     curInv.push(newInv[idx])
+//   }
+// }
+// console.log(curInv)
