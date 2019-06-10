56e49b7f1dd07c960ed00f44710cc1ae4100d45c
mrstevenaweiss <weiss.steven@gmail.com>
2019-06-10 12:33:34 -0400

    added partition function

---
+++ JavaScript/RosettaCode/partitionFunction.js
@@ -0,0 +1,13 @@
+
+https://www.whitman.edu/mathematics/cgt_online/book/section03.03.html
+
+function numberOfNames (num) {
+  // Good luck!
+  return true;
+}
+numberOfNames(5) should equal 7.
+numberOfNames(12) should equal 77.
+numberOfNames(18) should equal 385.
+numberOfNames(23) should equal 1255.
+numberOfNames(42) should equal 53174.
+numberOfNames(123) should equal 2552338241.
