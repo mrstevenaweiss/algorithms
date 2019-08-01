660b6d787a77af834b1eea8668847e8ab08ea949
Steven Weiss <weiss.steven@gmail.com>
2019-08-01 08:55:37 -0400

    finished 6 on Day 6

--- Euler_50/6_sum_square_diff.py
+++ Euler_50/6_sum_square_diff.py
@@ -1,73 +1,47 @@
 
 import time
 '''
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 '''
-def least_common_multiple():
-    """You need to know primes and the # of powers < N"""
-    # Rethinking an earlier attempt at Prime factorization 
-    first = 2**4
-    second = 3**2
-    return first*second*5*7*11*13*17*19
+def gauss_add(N):    
+    return (N*(N+1)) /  2
+
+def sum_squares(N):
+    sum = 0
+    for i in range(1, N+1):
+        # print(i)
+        sum += (i**2)
+    return sum
+
+def sum_square_diff():
+    N = 100
+
+    print('gauss squared', int(gauss_add(N)**2))
+    print( 'sum of', N, 'squares', sum_squares(N) )
+    
+    answer = gauss_add(N)**2 - sum_squares(N)
+    print('answer', answer)
   
 
 start = time.time()
-print( least_common_multiple() )
+sum_square_diff()
 end = time.time()
 print('Processed in ', end - start, 'seconds')
 
 
-# 1st approach
-# # # # a_list[0] = number % 2 != 0
-# # # #     a_list[1] = number % 3 != 0
-# # # #     a_list[2] = number % 4 != 0
-# # # #     a_list[3] = number % 5 != 0
-# # # #     a_list[4] = number % 6 != 0
-# # # #     a_list[5] = number % 7 != 0# # # #     a_list[6] = number % 8 != 0
-# # # #     a_list[7] = number % 9 != 0
-# # # #     a_list[8] = number % 10 != 0
-# # # #     a_list[9] = number % 11 != 0
-# # # #     a_list[10] = number % 12 != 0
-# # # #     a_list[11] = number % 13 != 0
-# # # #     a_list[12] = number % 14 != 0
-# # # #     a_list[13] = number % 15 != 0
-# # # #     a_list[14] = number % 16 != 0
-# # # #     a_list[15] = number % 17 != 0
-# # # #     a_list[16] = number % 18 != 0
-# # # #     a_list[17] = number % 19 != 0
-# # # #     a_list[18] = number % 20 != 0
-# # # #     lister = True in a_list
-# # # #     number += 2
-    
-# 2nd approach
-# # #   if number % 1 == 0:
-# # #     print('fourth loop')
-# # #     adder += 1
-# # #     if number % 2 == 0:
-# # #       adder += 2
-# # #       if number % 3 == 0:
-# # #         adder += 3
-# # #         if number % 4 == 0:
-# # #             adder += 4
-# # #             if number % 5 == 0:
-# # #                 pri
 
 
 
 
-
-
-
-The sum of the squares of the first ten natural numbers is,
-
-12 + 22 + ... + 102 = 385
-The square of the sum of the first ten natural numbers is,
-
-(1 + 2 + ... + 10)2 = 552 = 3025
-Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-
-Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 
             
