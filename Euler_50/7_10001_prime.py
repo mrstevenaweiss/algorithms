d7f5fb7df8545d2654ab9db99a8a1bfa8b3334a1
Steven Weiss <weiss.steven@gmail.com>
2019-08-03 20:21:40 -0400

    8_largest_product_series.py

--- Euler_50/7_10001_prime.py
+++ Euler_50/7_10001_prime.py
@@ -1,20 +1,21 @@
 
 import time
-import primes
+import primer
 
 '''
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?
 
 '''
-def get_prime(X):
-    x = primes.Primes()
-  
+# def get_prime():
+primes_list = primer.Primer()
+
+# for i in range(1000000):
+#     primes_list.is_prime(i)
+print(primes_list.cache, '\n')
 
 start = time.time()
-
-get_prime(5)
-
+print(primes_list.cache[2])
 end = time.time()
 print('Processed in ', end - start, 'seconds')
 
