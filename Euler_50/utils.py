daceff2f653c92ac5318afe652b58a0224f89ed3
Steven Weiss <weiss.steven@gmail.com>
2019-08-11 12:05:58 +0200

    added utils

--- Euler_50/utils.py
+++ Euler_50/utils.py
@@ -1,2 +1,24 @@
 
 import time
+
+
+
+def isqrt(n):
+    "Integer square root (rounds down)."
+    return int(n ** 0.5)
+
+
+def multiply(numbers):
+    "Multiply all the numbers together."
+    result = 1
+    for n in numbers:
+        result *= n
+    return result
+
+
+start = time.time()
+#function here
+end = time.time()
+print('Processed in ', end - start, 'seconds')
+
+
