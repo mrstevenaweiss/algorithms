d104ec64e23162da7c02e0049f4ded0c5103d67a
Steven Weiss <weiss.steven@gmail.com>
2019-08-14 17:31:15 +0200

    finished 18 on Day 18

--- Euler_50/18_max_path_sum.py
+++ Euler_50/18_max_path_sum.py
@@ -1,4 +1,6 @@
+import time 
 
+"""
 By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
 3
@@ -26,4 +28,74 @@ Find the maximum total from top to bottom of the triangle below:
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 
-NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. 
+However, Problem 67, is the same challenge with a triangle containing one-hundred rows; 
+it cannot be solved by brute force, and requires a clever method! ;o)
+"""
+
+
+triangle = [
+[75],
+[95, 64],
+[17, 47, 82],
+[18, 35, 87, 10],
+[20,  4, 82, 47, 65],
+[19,  1, 23, 75,  3, 34],
+[88,  2, 77, 73,  7, 63, 67],
+[99, 65,  4, 28,  6, 16, 70, 92],
+[41, 41, 26, 56, 83, 40, 80, 70, 33],
+[41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+[53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+[70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+[91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+[63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+[ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]
+]
+
+tri = [
+     [3],
+   [7, 4],
+  [2, 4, 6],
+[8, 5, 9, 3]
+]
+
+yo = []
+first = 0
+second = 1
+
+for list in triangle: 
+    if len(list) > 1:
+        bigger = max( list[first], list[second] )
+        yo.append(bigger)
+        currentIdx = list.index( bigger, first )
+        first = currentIdx
+        second = currentIdx+1
+    else:
+        yo.append(list[0])
+    
+# read in the data
+# rows = []
+# with open('problem-18-data') as f:
+#     for line in f:
+#         rows.append([int(i) for i in line.rstrip('\n').split(" ")])
+
+
+start = time.time()
+print(yo)
+# print(sum(yo))
+end = time.time()
+print('Processed in ', end - start, 'seconds')
+
+# Other approaches...
+# nombres = '''3
+# 7 4
+# 2 4 6
+# 8 5 9 3'''
+
+# nombres = [i for i in nombres.split("\n")]
+# print(nombres)
+
+# # nombres = [int(i) for i in nombres.split("\n")]
+# # sum(nombres)
+# # print "Top 10 digits of sum =", str(total)[:10]
+
