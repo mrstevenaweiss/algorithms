3b50db353f44abcb78e91687272664fcc450596c
Steven Weiss <weiss.steven@gmail.com>
2019-08-22 23:29:29 -0400

    finished euler 21

--- Euler_50/23_non_abundant_sums.py
+++ Euler_50/23_non_abundant_sums.py
@@ -1,16 +1,37 @@
 import time
+
 """
-A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. 
+For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
 
 A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 
-As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, 
+the smallest number that can be written as the sum of two abundant numbers is 24. 
+By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
+However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest 
+number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 
 """
 
+def uniq_factors(factor):
+    divider = 2
+    factors = [1]
 
+    if factor in primes:
+        return "prime"
+    else: 
+        for i in range(factor//2):
+            if factor % divider == 0:
+                factors.append(divider)
+                # factors.append(factor//divider)
+                divider += 1
+            else:
+                divider += 1
+        # print('factors', factors)
+        return factors
 
 start = time.time()
 end = time.time()
--- Euler_50/utils.py
+++ Euler_50/utils.py
@@ -17,6 +17,27 @@ def multiply(numbers):
         result *= n
     return result
 
+def largest_prime_factor(test):
+    nums = []
+    # test = 13195
+    divider = 2
+  
+    while divider <= test:
+      if test % divider != 0:
+        divider += 1
+      else:
+        test = test // divider
+        nums.append(divider)
+    return nums[-1]
+  
+def getDivisors(n): 
+    i = 1
+    divisors = []
+    while i <= n : 
+        if (n % i==0) : 
+            divisors.append(i) 
+        i = i + 1
+    return divisors
 
 start = time.time()
 #function here
